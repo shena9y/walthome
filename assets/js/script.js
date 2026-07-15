26a2f75e06b0a75c414168dd42df6ebd1a20d461
Mohammed Hamdy <119974638+shena9y@users.noreply.github.com>
2026-07-15 23:49:21 +0300

    Added the project files to the main

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ *Navbar Toggle In Mobile
+ */
+
+const /** {NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const /**{NodeElement} */ $navToggler =
+    document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+/**
+ * Headder scroll state
+ */
+
+const /**{NodeElement}*/ $header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", (e) => {
+  $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Add to favorite button toggle
+ */
+
+const /**{NodeList} */ $toggleBtns =
+    document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach(($toggleBtn) => {
+  $toggleBtn.addEventListener("click", () => {
+    $toggleBtn.classList.toggle("active");
+  });
+});
